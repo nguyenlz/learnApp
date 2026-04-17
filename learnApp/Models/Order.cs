80cc5244a02e1a7c05d4bfec92b85cdea9f847d9
nguyenlz <nbnguyen1100@gmail.com>
2026-04-17 14:54:22 +0700

    sửa model: thêm lưu trạng thái order, payment, thêm bảng site

--- learnApp/Models/Order.cs
+++ learnApp/Models/Order.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using learnApp.Enums;
 
 namespace learnApp.Models;
 
@@ -10,16 +11,25 @@ public partial class Order
     public int OrderId { get; set; }
 
     public int CustomerId { get; set; }
-
     public int EmployeeId { get; set; }
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
     public DateTime? OrderDate { get; set; }
 
-    public decimal? TotalAmount { get; set; }
+    public decimal TotalAmount { get; set; }
+    public decimal PaidAmount { get; set; } = 0;
+
+    public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Unpaid;
+
+    // (optional) công trình
+    public int? SiteId { get; set; }
     [ValidateNever]
     public virtual Customer Customer { get; set; } = null!;
     [ValidateNever]
     public virtual Employee Employee { get; set; } = null!;
+    public virtual Site Site { get; set; }
     [ValidateNever]
     public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
 }
---
+++ learnApp/Models/Site.cs
@@ -0,0 +1,13 @@
+﻿namespace learnApp.Models
+{
+    public class Site
+    {
+        public int SiteId { get; set; }
+        public string Name { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
